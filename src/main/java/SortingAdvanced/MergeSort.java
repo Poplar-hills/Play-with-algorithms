324fa95f82626d11195c1631e3b03a95d271305f
Max Jiang <maxjiang23326@gmail.com>
2019-01-25 07:46:15 +0800

    docs: add more comments

--- src/main/java/SortingAdvanced/MergeSort.java
+++ src/main/java/SortingAdvanced/MergeSort.java
@@ -8,9 +8,8 @@ import static Utils.Helpers.*;
 * 归并排序（Merge Sort）
 * - 复杂度是 O(nlogn)
 * - 归并排序递归地：
-*   1. 将数组进行二分
-*   2. 分别对左右两半进行排序
-*   3. 再将排序后的两部分归并起来
+*   1. 将数组进行二分（从上到下）
+*   2. 对每一层的每一部分进行归并（从下到上）
 *
 *              不断进行二分                第一次归并                第二次归并                第三次归并
 *   Level0   8 6 2 3 1 5 7 4          8 6 2 3 1 5 7 4          8 6 2 3 1 5 7 4          1 2 3 4 5 6 7 8
@@ -21,7 +20,7 @@ import static Utils.Helpers.*;
 * - 归并排序的思想：
 *   - 有 n 个元素的数组可以进行 log(n) 次二分操作，共分出 log(n) 个层级。
 *   - 如果每层的排序和归并过程能在 O(n) 的复杂度内完成，则该算法的整体复杂度就是 O(nlogn)。
-*   注：这个思想也是所有 O(nlogn) 复杂度的算法的来源 —— 通过二分达到 log(n) 的层级，在每层内用 O(n) 的算法来做事情。
+*   注：这个思想也是所有 O(nlogn) 复杂度的算法的来源 —— 通过二分获得 log(n) 个层级，在每层内使用 O(n) 的算法来做事情。
 *
 * - 归并排序的优化：
 *   对于近乎有序的数组，归并排序要慢于插入排序，因为归并排序不能像插入排序那样退化成 O(n) 的复杂度。但是仍然可以进行优化：
@@ -29,15 +28,13 @@ import static Utils.Helpers.*;
 * */
 
 public class MergeSort {
-    public static void sort(Comparable[] arr) {
+    public static void sort(Comparable[] arr) {  // 归并排序中的递归是要对数组的每一段区域进行处理，因此设计递归函数时要传入左右边界
         sort(arr, 0, arr.length - 1);
     }
 
     // 递归地对 arr[l...r] 的范围（前闭后闭）进行排序
     private static void sort(Comparable[] arr, int l, int r) {
-        if (l >= r)
-            return;
-
+        if (l >= r) return;          // 递归终止条件
         int mid = (r - l) / 2  + l;  // 也可以写成 (l + r) / 2，但是可能整型溢出
         sort(arr, l, mid);
         sort(arr, mid + 1, r);
