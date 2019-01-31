44079b052b2d9117fa73f18f9c17df93f6518280
Max Jiang <maxjiang23326@gmail.com>
2019-01-31 08:04:46 +0800

    feat: add analyse for performance

--- src/main/java/SortingAdvanced/QuickSort.java
+++ src/main/java/SortingAdvanced/QuickSort.java
@@ -21,6 +21,26 @@ package SortingAdvanced;
 *      |_____|  |  |________|
 *         |     |      |
 *        < v    v     > v
+*
+* - 性能分析：
+*   - 虽然归并排序和快速排序都是 O(nlogn) 级别的，但是快速排序之所以更快是因为快速排序的那个常数更小。具体表现在：
+*     1. 快速排序每一层的比较次数比归并排序少。
+*     2. 更不用提归并排序的 merge 过程，还需要一个辅助空间，将所有的元素都挪一边，而快速排序则完全是原地的。
+*   - 但是当面对近乎有序的数组时，未经优化的快速排序的性能比归并排序低很多，并且对于十万百万级别的数据量会出现栈溢
+*     出的问题，这是因为这两种算法都是将数组进行二分后再分别处理，但在分法不同：
+*     1. 归并排序是标准的二分，因此最后得到的递归树的平衡性是很好的；
+*     2. 而快速排序是根据一个标定元素进行 partition，分出的两个区域很可能大小不一样，因此最后得到的递归树的平衡
+*        性较差。同时，树的高度也不一定是 log(n)，因为在最差情况下（数组完全有序），partition 选取的的标定元素就
+*        是数组中的最小值，因此二分结果是左边区域完全没有元素，右边区域包含所有元素的情况：
+*                  [1  2  3  4]
+*                      /    \
+*                    []    [2  3  4]
+*                            /   \
+*                          []   [3  4]
+*                                /  \
+*                              []   [4]
+*        这时，快速排序会退化成 O(n^2) 的复杂度（整棵树的高度是 n，对每层处理的复杂度是 O(n)），因此性能低下。
+*
 * */
 
 import static Utils.Helpers.*;
@@ -61,5 +81,10 @@ public class QuickSort {
         Integer[] arr2 = arr1.clone();
         timeIt(arr1, QuickSort::sort);  // 这一版的 quick sort 已经比 merge sort 快将近三分之一了
         timeIt(arr2, MergeSort::sort);
+
+        Integer[] arr3 = generateNearlyOrderedArr(10000, 0);
+        Integer[] arr4 = arr3.clone();
+        timeIt(arr3, QuickSort::sort);
+        timeIt(arr4, MergeSortForNearlyOrderedArr::sort);  // 比普通的 MergeSort 快一些
     }
 }
