be05e522db930e2fb8b4904f46ee65687b425b4c
Max Jiang <maxjiang23326@gmail.com>
2019-02-01 13:49:03 +0800

    feat: add method to verify if an array is ordered

--- src/main/java/Utils/Helpers.java
+++ src/main/java/Utils/Helpers.java
@@ -62,4 +62,12 @@ public class Helpers {
 
         return arr;
     }
+
+    public static boolean isArrOrdered(Comparable[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].compareTo(arr[i + 1]) > 0)
+                return false;
+        }
+        return true;
+    }
 }
