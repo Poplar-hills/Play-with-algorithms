55fff5a80ea0fb9737328d3807f2f775f78f015d
Max Jiang <maxjiang23326@gmail.com>
2019-02-17 07:12:54 +0800

    feat: finish ceil & floor methods for BinarySearch

--- src/main/java/BST/BinarySearch.java
+++ src/main/java/BST/BinarySearch.java
@@ -11,13 +11,14 @@ import static Utils.Helpers.log;
 * */
 
 public class BinarySearch {
+    // 二分查找法
     public static int sort(Comparable[] arr, Comparable e) {
         return binarySort(arr, e, 0, arr.length - 1);
     }
 
     private static int binarySort(Comparable[] arr, Comparable e, int l, int r) {
         if (l > r) return -1;
-        int mid = (r - l) / 2 + l;  // 防止极端情况下的整形溢出
+        int mid = (r - l) / 2 + l;
         if (e.compareTo(arr[mid]) < 0)
             return binarySort(arr, e, l, mid - 1);
         else if (e.compareTo(arr[mid]) > 0)
@@ -26,11 +27,70 @@ public class BinarySearch {
             return mid;
     }
 
+    /*
+     * 基于二分查找法，在有序数组 arr 中查找 e：
+     *  - 如果找到 e，返回最后一个 e 相应的索引；
+     *  - 如果没有找到 e, 返回比 e 大的最小值的索引，如果这个最小值有多个，返回最小的索引；
+     *  - 如果这个 e 比整个数组的最大值还大，则不存在这个 e 的 ceil 值，返回整个数组的元素个数 n。
+     */
+    public static int ceil(Comparable[] arr, Comparable e) {
+        return ceil(arr, e, 0, arr.length - 1);
+    }
+
+    private static int ceil(Comparable[] arr, Comparable e, int l, int r) {
+        if (l > r) return l;
+        int mid = (r - l) / 2 + l;
+        if (e.compareTo(arr[mid]) < 0)
+            return ceil(arr, e, l, mid - 1);
+        else if (e.compareTo(arr[mid]) > 0)
+            return ceil(arr, e, mid + 1, r);
+        else {
+            int t = mid;
+            while (arr[t] == arr[mid])
+                t += 1;
+            return t - 1;
+        }
+    }
+
+    /*
+     * 基于二分查找法，在有序数组 arr 中查找 e：
+     *  - 如果找到 e，返回最后一个 e 相应的索引；
+     *  - 如果没有找到 e, 返回比 e 小的最大值的索引，如果这个最大值有多个，返回最大的索引；
+     *  - 如果这个 e 比整个数组的最小值还小，则不存在这个 e 的 floor 值，返回 -1。
+     */
+    public static int floor(Comparable[] arr, Comparable e) {
+        return floor(arr, e, 0, arr.length - 1);
+    }
+
+    private static int floor(Comparable[] arr, Comparable e, int l, int r) {
+        if (l > r) return r;
+        int mid = (r - l) / 2 + l;
+        if (e.compareTo(arr[mid]) < 0)
+            return floor(arr, e, l, mid - 1);
+        else if (e.compareTo(arr[mid]) > 0)
+            return floor(arr, e, mid + 1, r);
+        else {
+            int t = mid;
+            while (arr[t] == arr[mid])
+                t -= 1;
+            return t + 1;
+        }
+    }
+
     public static void main(String[] args) {
-        Integer[] arr = {1, 3, 4, 7, 9, 10};
-        log(sort(arr, 0));
-        log(sort(arr, 3));
-        log(sort(arr, 7));
-        log(sort(arr, 18));
+        Integer[] arr1 = {1, 3, 4, 7, 9, 10};
+        log(String.format("%d, %d, %d", sort(arr1, 3), sort(arr1, 7), sort(arr1, 18)));
+
+        Integer[] arr2 = {1, 2, 2, 2, 4, 5, 7, 9, 10};  // 数据集中有多个2，此时 ceil 应返回最后一个2的索引3，floor 应返回第一个2的索引1
+        log(String.format("ceil: %d; floor: %d", ceil(arr2, 2), floor(arr2, 2)));
+
+        Integer[] arr3 = {1, 3, 4, 7, 9, 10};  // 数据集中没有5，此时 ceil 应返回7的索引3，floor 应返回4的索引2
+        log(String.format("ceil: %d floor: %s", ceil(arr3, 5), floor(arr3, 5)));
+
+        Integer[] arr4 = {1, 3, 4, 7, 9, 10};  // 数据集中没有12，且12 > 数组最大值，此时 ceil 应返回数组元素个数6，floor 应返回最大值的索引5
+        log(String.format("ceil: %d floor: %s", ceil(arr4, 12), floor(arr4, 12)));
+
+        Integer[] arr5 = {1, 3, 4, 7, 9, 10};  // 数据集中没有0，且0 < 数组最小值，此时 ceil 应返回最小值的索引0，floor 应返回-1
+        log(String.format("ceil: %d floor: %s", ceil(arr5, 0), floor(arr5, 0)));
     }
 }
