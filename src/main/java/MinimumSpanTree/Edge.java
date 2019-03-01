b7e8014f29a652d0d87f11c5d5d987e388e802ff
Max Jiang <maxjiang23326@gmail.com>
2019-03-01 23:05:31 +0800

    feat: add Edge for weighted graph

---
+++ src/main/java/MinimumSpanTree/Edge.java
@@ -0,0 +1,34 @@
+package MinimumSpanTree;
+
+public class Edge<Weight extends Number & Comparable> implements Comparable<Edge> {
+    private int a, b;  // 边上的两个顶点（对于有向图来说，该边是从 a 指向 b）
+    private Weight weight;  // 边的权值
+
+    public Edge(int a, int b, Weight weight) {
+        this.a = a;
+        this.b = b;
+        this.weight = weight;
+    }
+
+    public Edge() { }
+
+    public int v() { return a; }
+
+    public int w() { return b; }
+
+    public Weight weight() { return weight; }
+
+    public int theOther(int x) {  // 给定边上的一个顶点，返回另一个顶点
+        if (x != a && x != b)
+            throw new IllegalArgumentException("theOther failed. x is not a vertex on the edge");
+        return x == a ? b : a;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        int comp = weight.compareTo(other.weight());
+        if (comp < 0) return -1;
+        else if (comp > 0) return 1;
+        else return 0;
+    }
+}
