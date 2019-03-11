a854800d9d1e1d1511e9b970cee4d3675514ffd0
Max Jiang <maxjiang23326@gmail.com>
2019-03-11 13:13:03 +0800

    refactor: modify MaxHeap to use static data structure under the hood

--- src/main/java/Heap/MaxHeap.java
+++ src/main/java/Heap/MaxHeap.java
@@ -1,5 +1,7 @@
 package Heap;
 
+import java.util.Arrays;
+
 import static Utils.Helpers.log;
 
 /*
@@ -7,7 +9,7 @@ import static Utils.Helpers.log;
 * */
 
 public class MaxHeap<E extends Comparable<E>> {
-    private E[] data;  // 这里使用静态数据结构实现了不能扩容的最大堆，若需要扩容则需使用 List，或自行为 data 添加 resize 方法
+    private E[] data;  // 这里使用数组实现最大堆，因此不能扩容，若要扩容则需使用动态数据结构实现，或自行为 data 添加 resize 方法
     private int size;
 
     public MaxHeap(int capacity) {
@@ -16,6 +18,7 @@ public class MaxHeap<E extends Comparable<E>> {
 
     public MaxHeap(E[] arr) {  // heapify
         int n = arr.length;
+        size = 0;
 
         data = (E[]) new Comparable[n];
         for (int i = 0; i < n; i++)
@@ -52,10 +55,10 @@ public class MaxHeap<E extends Comparable<E>> {
     }
 
     private void siftDown(int k) {
-        while (getLeftChildIndex(k) < getSize()) {  // 只要左孩子的索引 < 元素个数就说明还没到达叶子节点，可以继续循环
+        while (getLeftChildIndex(k) < size) {  // 如果左孩子存在（没越界）就继续循环
             // 找到位于 k 的节点的左右孩子中较大的那个的索引
             int i = getLeftChildIndex(k);
-            if (i + 1 < getSize() && data[i + 1].compareTo(data[i]) > 0)  // i+1 是右孩子的索引，i+1 < getSize() 是要保证有右孩子
+            if (i + 1 < size && data[i + 1].compareTo(data[i]) > 0)  // i+1 是右孩子的索引，i+1 < size 是要保证有右孩子
                 i += 1;  // i 保存了左右孩子中值较大的那个的索引
 
             // 用父节点与较大的那个比，如果父节点大则 break loop，否则 swap（只有用较大的子节点跟父节点比才能保证 swap 之后换上来的新父节点比两个子节点都大，保证最大堆性质不被破坏）
@@ -70,16 +73,17 @@ public class MaxHeap<E extends Comparable<E>> {
     private E findMax() { return data[0]; }  // 找到最大值，相当于 priority queue 的 peek 方法
 
     public void insert(E e) {
-        data[data.length - 1] = e;
-        siftUp(getSize() - 1);
+        data[size] = e;
+        size++;
+        siftUp(size - 1);
     }
 
     public E extractMax() {
-        E ret = data[0];     // 先保存最大值
-        int lastIndex = getSize() - 1;
-        data[0] = data[lastIndex];  // 取数组的最后一个元素覆盖第一个元素
-        data[lastIndex] = null;  // 移除最大值元素（注：以上这三行不能写成 data.set(0, data.remove(getSize() - 1)); 因为当 data 中只剩一个元素时，remove 掉之后就无法再 set）
-        siftDown(0);          // 对新的第一个元素进行下沉操作
+        E ret = data[0];           // 先保存最大值
+        data[0] = data[size - 1];  // 取数组的最后一个元素覆盖第一个元素
+        data[size - 1] = null;     // 移除最大值元素
+        size--;
+        siftDown(0);            // 对新的第一个元素进行下沉操作
         return ret;
     }
 
@@ -90,13 +94,13 @@ public class MaxHeap<E extends Comparable<E>> {
         return ret;
     }
 
-    public int getSize() { return data.length; }
+    public int getSize() { return size; }
 
     public boolean isEmpty() { return getSize() == 0; }
 
     @Override
     public String toString() {
-        return data.toString();
+        return Arrays.toString(data);
     }
 
     public static void main(String[] args) {
