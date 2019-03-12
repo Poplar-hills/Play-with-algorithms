65edc768b6eb779243e9e5b20996de32bbedd2ee
Max Jiang <maxjiang23326@gmail.com>
2019-03-12 22:46:30 +0800

    feat: add UnionFind version 1

--- src/main/java/MinimumSpanningTree/AuxiliaryDataStructure/UnionFind.java
+++ src/main/java/MinimumSpanningTree/AuxiliaryDataStructure/UnionFind.java
@@ -1,5 +1,9 @@
 package MinimumSpanningTree.AuxiliaryDataStructure;
 
+import java.util.Arrays;
+
+import static Utils.Helpers.log;
+
 /*
 * 并查集（作为 KruskalMST 的辅助数据结构）
 *
@@ -7,5 +11,52 @@ package MinimumSpanningTree.AuxiliaryDataStructure;
 * */
 
 public class UnionFind {
+    private int[] setIds;
 
+    public UnionFind(int size) {
+        setIds = new int[size];
+        for (int i = 0; i < setIds.length; i++)
+            setIds[i] = i;
+    }
+
+    private int find(int p) {
+        if (p < 0 || p >= setIds.length)
+            throw new IllegalArgumentException("find failed. p is out of bound.");
+        return setIds[p];
+    }
+
+    public boolean isConnencted(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int pSetId = find(p);
+        int qSetId = find(q);
+
+        if (pSetId == qSetId)
+            return;
+
+        for (int i = 0; i < setIds.length; i++) {
+            if (find(i) == pSetId)
+                setIds[i] = qSetId;
+        }
+    }
+
+    public int getSize() { return setIds.length; }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(setIds);
+    }
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(5);
+
+        uf.union(2, 4);
+        log(uf);
+        uf.union(4, 0);
+        log(uf);
+
+        log(uf.isConnencted(2, 0));
+    }
 }
