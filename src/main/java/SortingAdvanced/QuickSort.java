ed37d7ab7368d82e8c18c0dd7af053a0c0e7543d
Max Jiang <maxjiang23326@gmail.com>
2019-03-24 19:04:07 +0800

    docs: QuickSort better explained

--- src/main/java/SortingAdvanced/QuickSort.java
+++ src/main/java/SortingAdvanced/QuickSort.java
@@ -22,21 +22,21 @@ package SortingAdvanced;
 *       1. 若 arr[i] > v，则放着不动就可以归入 > v 的区间；
 *       2. 若 arr[i] <= v，则 swap(arr[i], arr[j+1]] 即可归入 < v 的区间。
 *     - 完整过程如下：
-*       4  6  2  3  5  7  1  8   以4作为 pivot，访问 6，∵ 6 > v，∴ 放着不动，i++
+*       4  6  5  2  3  7  1  8   以4作为 pivot，访问 6，∵ 6 > v，∴ 放着不动，i++
 *       lj i
-*       4  6  2  3  5  7  1  8   访问 2，∵ 2 < v，∴ 对2和6进行 swap，i++，j++
+*       4  6  5  2  3  7  1  8   访问 5，∵ 5 > v，∴ 放着不动，i++
 *       lj    i
-*       4  2  6  3  5  7  1  8   访问 3，∵ 3 < v，∴ 对3和6进行 swap，i++，j++
-*       l  j     i
-*       4  2  3  6  5  7  1  8   访问 5，∵ 5 > v，∴ 放着不动，i++
-*       l     j     i
+*       4  6  5  2  3  7  1  8   访问 2，∵ 2 < v，∴ swap(i, j+1)，即对2和6进行 swap，i++，j++
+*       lj       i
+*       4  2  5  6  3  7  1  8   访问 3，∵ 3 < v，∴ swap(i, j+1)，即对3和5进行 swap，i++，j++
+*       l  j        i
 *       4  2  3  6  5  7  1  8   访问 7，∵ 7 > v，∴ 放着不动，i++
 *       l     j        i
-*       4  2  3  6  5  7  1  8   访问 1，∵ 1 < v，∴ 对1和6进行 swap，i++，j++
+*       4  2  3  6  5  7  1  8   访问 1，∵ 1 < v，∴ swap(i, j+1)，即对1和6进行 swap，i++，j++
 *       l     j           i
-*       4  2  3  1  5  7  6  8   访问 8，∵ 8 > v，∴ 放着不动，i++，循环终止，对 l 和 j 所指元素进行 swap
+*       4  2  3  1  5  7  6  8   访问 8，∵ 8 > v，∴ 放着不动，i++，循环终止，swap(l, j)
 *       l        j           i
-*       1  2  3  4  5  7  6  8   一次 partition 结束
+*       1  2  3  4  5  7  6  8   第一次 partition 结束
 *       |_____|  |  |________|
 *          |     |      |
 *         < v    v     > v
@@ -56,17 +56,40 @@ public class QuickSort {
         sort(arr, p + 1, r);
     }
 
-    // 返回 p, 使得 arr[l...p-1] 中的元素都 < arr[p]；arr[p+1...r] 中的元素都 > arr[p]
     private static int partition(Comparable[] arr, int l, int r) {
-        Comparable v = arr[l];      // 标定元素 pivot
-        int j = l;                  // j 指向小于 v 的最后一个元素，即 arr[l+1...j] < v；arr[j+1...r] > v。最开始没有元素 < v，因此指向 l
+        Comparable v = arr[l];
+        int j = l;
         for (int i = l + 1; i <= r; i++) {
             if (arr[i].compareTo(v) < 0)
-                swap(arr, i, ++j);  // 与大于 v 的第一个元素进行 swap
+                swap(arr, i, ++j);
         }
-        swap(arr, l, j);            // 将 v 放到正确的位置（j）上
-        return j;                   // 返回 v 的索引
+        swap(arr, l, j);
+        return j;
     }
+
+
+//    public static void sort(Comparable[] arr) {
+//        sort(arr, 0, arr.length - 1);
+//    }
+//
+//    private static void sort(Comparable[] arr, int l, int r) {
+//        if (l >= r) return;
+//        int p = partition(arr, l, r);
+//        sort(arr, l, p - 1);
+//        sort(arr, p + 1, r);
+//    }
+//
+//    // 返回 p, 使得 arr[l...p-1] 中的元素都 < arr[p]；arr[p+1...r] 中的元素都 > arr[p]
+//    private static int partition(Comparable[] arr, int l, int r) {
+//        Comparable v = arr[l];      // 标定元素 pivot
+//        int j = l;                  // j 指向小于 v 的最后一个元素，即 arr[l+1...j] < v；arr[j+1...r] > v。最开始没有元素 < v，因此指向 l
+//        for (int i = l + 1; i <= r; i++) {
+//            if (arr[i].compareTo(v) < 0)
+//                swap(arr, i, ++j);  // 与大于 v 的第一个元素进行 swap
+//        }
+//        swap(arr, l, j);            // 将 v 放到正确的位置（j）上
+//        return j;                   // 返回 v 的索引
+//    }
 
     public static void main(String[] args) {
         Integer[] arr = generateRandomIntArr(20);
