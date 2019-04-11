fc3d4307e6afde02b2f6e05e079765a8a833cfe3
Max Jiang <maxjiang23326@gmail.com>
2019-04-11 20:44:20 +0800

    docs: add comment

--- src/main/java/SortingSpecialised/CountingSort2.java
+++ src/main/java/SortingSpecialised/CountingSort2.java
@@ -14,6 +14,10 @@ import static Utils.Helpers.log;
 *
 * - 复杂度：
 *   时间和空间复杂度仍然是 O(n+k) 和 O(k)，但其中 k 变成了最大元素和最小元素之差。
+*
+* - 局限性：
+*   CountingSort1 和 CountingSort2 都不是稳定排序（即排序后，等值的两个元素的相对顺序可能会被颠倒），可以进行进一步优化，
+*   SEE: 微信搜索"漫画：什么是计数排序？"。
 * */
 
 public class CountingSort2 {
