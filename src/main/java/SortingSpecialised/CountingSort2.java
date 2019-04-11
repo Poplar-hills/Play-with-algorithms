f9718283fabc07c108d75f36666be7e422a29b76
Max Jiang <maxjiang23326@gmail.com>
2019-04-11 20:24:16 +0800

    docs: format

--- src/main/java/SortingSpecialised/CountingSort2.java
+++ src/main/java/SortingSpecialised/CountingSort2.java
@@ -7,8 +7,10 @@ import static Utils.Helpers.log;
 *
 * - CountingSort1 的缺陷：
 *   对于 arr = [95, 94, 91, 98, 99] 这样的数组来说，仍然需要构造大小为100的 buckets 数组，因为 arr 中最小元素为 91，
-*   因此从0到90的位置都浪费掉了，空间效率低下。对此优化方式是，以 arr 最大元素和最小元素之差+1作为 bucket 数组的长度，
-*   同时用最小元素作为填充 arr 时的偏移量，代码见 countingSort2。
+*   因此从0到90的位置都浪费掉了，空间效率低下。
+*
+* - 对此优化方式:
+*   以 arr 最大元素和最小元素之差+1作为 bucket 数组的长度，同时用最小元素作为填充 arr 时的偏移量，代码见 countingSort2。
 * */
 
 public class CountingSort2 {
